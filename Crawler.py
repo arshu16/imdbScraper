6bdd025be27eec24a22c1f073ad0cd571115bee2
Ishmeet Singh Bindra <ishmeet.bindra@delhivery.com>
2017-07-22 21:47:54 +0530

    You can add genres now.

--- Crawler.py
+++ Crawler.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Script that gets the top 10 most popular titles from imdb in an year range
 # USAGE
-# Command: python /scriptName/ *startYear* *endYear*
+# Command: python /scriptName/ *startYear* *endYear* *genres*
+# Defaults: genre is None, for multiple genres put 'horror,action,etc.'
 # Defaults: startYear = 2015; endYear = currentYear
 from bs4 import BeautifulSoup
 import json
@@ -29,8 +30,11 @@ def parse(url):
         soup = BeautifulSoup(movieHomePage, 'lxml')
         cover_div = soup.find('div', attrs={"class": "poster"})
         cover_url = (cover_div.find('img'))['src']
-        rating = float(movieInfo.find(
-            'div', attrs={'class': 'ratings-imdb-rating'}).find('strong').get_text())
+        ratingContainer = movieInfo.find(
+            'div', attrs={'class': 'ratings-imdb-rating'})
+        rating = ''
+        if ratingContainer:
+            rating = float(ratingContainer.find('strong').get_text())
         genre = movieInfo.find('span', attrs={'class': 'genre'})
         if genre:
             genre = genre.get_text().strip()
@@ -47,7 +51,7 @@ def parse(url):
             data['genre'] = genre.split(', ')
             data['imgLink'] = cover_url
             data['rating'] = rating
-            data['link'] = movieHomePage
+            data['link'] = movieLink
             parsed[title] = data
     return parsed
 
@@ -58,13 +62,16 @@ arguments = sys.argv[1:]
 startYear = int((arguments and arguments[0]) or 2015)  # Start year
 endYear = int((arguments and len(arguments) > 1 and arguments[1]
                ) or datetime.datetime.now().year)  # endyear
+genre = (arguments and arguments[2])
 try:
     for i in range(startYear, endYear + 1):
 
         print("Year is: " + str(i))
         year_data = {}
-        year_data.update(parse('http://www.imdb.com/search/title?release_date='
-                               + str(i) + '&ref_=adv_nxt&sort=moviemeter,asc'))
+        scrapeLink = 'http://www.imdb.com/search/title?release_date=' + \
+            str(i) + '&ref_=adv_nxt&sort=moviemeter,asc&genres=' + genre
+        print(scrapeLink)
+        year_data.update(parse(scrapeLink))
         to_write[i] = year_data
 except Exception, e:
     print("Error!", str(e))
--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Script that gets the top 10 most popular titles from imdb in an year range
 
 # Returned structre
 
+CLI Options: 
+``` python Crawler.py *startYear* *endYear* *genres* ```
+
 ```
 data = {
   title: { 
